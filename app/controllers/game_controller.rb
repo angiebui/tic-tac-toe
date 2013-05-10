0971e2d39ff2ccba42415fa9558e33a969765e38
stephenitis <nguyenhstephen@gmail.com>
2013-05-10 12:32:20 -0700

    create initial game board

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 get '/game/:id' do
   @game = Game.find(params[:id])
 
@@ -11,6 +5,13 @@ get '/game/:id' do
 end
 
 post '/game/:id/move' do
-  
+  Move.create(player_id: session[:player_id], coord: params[:coord], game_id: params[:id])
+end
 
+post '/game/:id/update' do
+  content_type :json
+  game = Game.find(params[:id])
+  move = game.moves.last #the last player to move
+
+  {coord: move.coord, previousPlayer: move.player_id}.to_json
 end
--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,4 @@
-Game = {id:1, letter:'X'} // Player # and Corresponding Letter and Game id
+Game = {id:1, letter:'X', playerId: 1} // Player # and Corresponding Letter and Game id
 
 
 var updateBoard = function(clickedBox){
@@ -11,6 +11,7 @@ var updateBoard = function(clickedBox){
 $(document).ready(function(){
   $('.clickable').click(function(){
     updateBoard($(this));
-
+    var data = {coord:$(this).attr('id')}
+    $.post('/game/' + Game.id + '/move', data)
   });
 });
