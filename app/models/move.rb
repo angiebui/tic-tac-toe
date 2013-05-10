0875c8c6231444ec4366fcc86ea80cb6ce4a61dd
stephenitis <nguyenhstephen@gmail.com>
2013-05-10 16:20:39 -0700

    player moves method

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,4 @@
 class Move < ActiveRecord::Base
-  belongs_to :player
+  # belongs_to :player
   belongs_to :game
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
 class Player < ActiveRecord::Base
   # Remember to create a migration!
+
+  def moves(game_id)
+    Move.where('game_id = ? and player_id = ?', game_id, self.id).map{ |move| move.coord }
+  end
 end
